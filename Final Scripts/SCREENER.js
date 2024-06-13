44a2f95224e4568f978fadede04101cdc8b4f36a
Tasawar Abbas <tabbas@stellatechnology.com>
2024-06-13 15:47:03 +0500

    final Screener Script

--- Final Scripts/SCREENER.js
+++ Final Scripts/SCREENER.js
@@ -1,5 +1,5 @@
 function dataAssociationWithEvent(clientCollection, userCollection, screenerCollection, detailCollection, summaryCollection, event, timelineData, currentTenantId, batchSize) {
-    var allTenantsInfo = db.getSiblingDB("dev-qhn-ninepatch-agency").getCollection("tenant").find({}, { name: 1 }).toArray().reduce((accumilator, tenant) => {
+    let allTenantsInfo = db.getSiblingDB("dev-qhn-ninepatch-agency").getCollection("tenant").find({}, { name: 1 }).toArray().reduce((accumilator, tenant) => {
         accumilator[tenant._id] = tenant.name;
         return accumilator;
     }, {});
@@ -7,20 +7,20 @@ function dataAssociationWithEvent(clientCollection, userCollection, screenerColl
         accumilator[user._id.toString()] = user;
         return accumilator;
     }, {});
-    var totalScreeners = db.getCollection(screenerCollection).countDocuments();
+    let totalScreeners = db.getCollection(screenerCollection).countDocuments();
     for (let skipValue = 0; skipValue <= totalScreeners; skipValue = skipValue + batchSize) {
-        var dataAssociationDetailedDocumentsList = [];
-        var dataAssociationSummaryDocumentsList = [];
-        var screenersList = db.getCollection(screenerCollection).find({}).skip(skipValue).limit(batchSize).toArray();
+        let dataAssociationDetailedDocumentsList = [];
+        let dataAssociationSummaryDocumentsList = [];
+        let screenersList = db.getCollection(screenerCollection).find({}).skip(skipValue).limit(batchSize).toArray();
         screenersList.forEach(screener => {
-            let client = db.getCollection(clientCollection).findOne({ _id: ObjectId(screener ?.clientId) });
+            let client = db.getCollection(clientCollection).findOne({ _id: ObjectId(screener?.clientId) });
             if (client) {
                 if (screener.createdBy && screener.lastModifiedBy) {
-                    const createrUser = allUsers[screener ?.createdBy];
-                    const modifierUser = allUsers[screener ?.lastModifiedBy];
+                    const createrUser = allUsers[screener?.createdBy];
+                    const modifierUser = allUsers[screener?.lastModifiedBy];
                     if (createrUser && modifierUser) {
                         if (!createrUser.defaultTenantId) {
-                            if (createrUser.tenantIds ?.length > 0) {
+                            if (createrUser.tenantIds?.length > 0) {
                                 createrUser.defaultTenantId = createrUser.tenantIds[0];
                             } else {
                                 createrUser.defaultTenantId = currentTenantId;
@@ -34,19 +34,19 @@ function dataAssociationWithEvent(clientCollection, userCollection, screenerColl
                                 modifierUser.defaultTenantId = currentTenantId;
                             }
                         }
-                        var createrUserTenantName = allTenantsInfo[createrUser.defaultTenantId];
-                        var modifierUserTenantName = allTenantsInfo[modifierUser.defaultTenantId];
-                        var screenerDetailObject = {
+                        let createrUserTenantName = allTenantsInfo[createrUser.defaultTenantId];
+                        let modifierUserTenantName = allTenantsInfo[modifierUser.defaultTenantId];
+                        let screenerDetailObject = {
                             "client": {
                                 "_id": client._id,
                                 "firstName": client.firstName,
                                 "lastName": client.lastName
                             },
                             "user": {
-                                "id": screener ?.createdBy,
-                                "firstName": createrUser ?.firstName,
-                                "lastName": createrUser ?.lastName,
-                                "fullName": createrUser ?.firstName + " " + createrUser ?.lastName,
+                                "id": screener?.createdBy,
+                                "firstName": createrUser?.firstName,
+                                "lastName": createrUser?.lastName,
+                                "fullName": createrUser?.firstName + " " + createrUser?.lastName,
                             },
                             "agency": {
                                 "_id": createrUser.agency._id,
@@ -59,24 +59,24 @@ function dataAssociationWithEvent(clientCollection, userCollection, screenerColl
                             "sourceId": screener._id.toString(),
                             "timelineData": timelineData,
                             "status": "ACTIVE",
-                            "createdBy": screener ?.createdBy,
-                            "createdAt": screener ?.createdAt,
+                            "createdBy": screener?.createdBy,
+                            "createdAt": screener?.createdAt,
                             "lastModifiedBy": screener.lastModifiedBy,
                             "lastModifiedAt": screener.lastModifiedAt,
                             "dataStatus": "ACTIVE",
                             "dataAudit": {
                                 "created": {
                                     "when": screener.createdAt,
-                                    "tenantId": createrUser ?.defaultTenantId,
+                                    "tenantId": createrUser?.defaultTenantId,
                                     "tenantName": createrUserTenantName,
                                     "entityId": createrUser.agency._id.toString(),
                                     "entityName": createrUser.agency.name,
-                                    "userId": createrUser ?.createdBy,
+                                    "userId": createrUser?.createdBy,
                                     "userFullName": createrUser.firstName + " " + createrUser.lastName
                                 },
                                 "updated": {
                                     "when": screener.lastModifiedAt,
-                                    "tenantId": modifierUser ?.defaultTenantId,
+                                    "tenantId": modifierUser?.defaultTenantId,
                                     "tenantName": modifierUserTenantName,
                                     "entityId": modifierUser.agency._id.toString(),
                                     "entityName": modifierUser.agency.name,
@@ -86,11 +86,12 @@ function dataAssociationWithEvent(clientCollection, userCollection, screenerColl
                             },
                             "_class": "dataAssociationDetail"
                         };
+                        dataAssociationDetailedDocumentsList.push(screenerDetailObject);
 
                         let clientSummaryObject = db.getCollection(summaryCollection).findOne({ "client._id": client._id });
                         //                { associations: { $elemMatch: { "user.id": screener.createdBy } } }
                         if (clientSummaryObject) {
-                            let userAssociated = db.getCollection(summaryCollection).findOne({ $and: [{ _id: clientSummaryObject._id }, { associations: { $elemMatch: { "user.id": screener ?.createdBy } } }] });
+                            let userAssociated = db.getCollection(summaryCollection).findOne({ $and: [{ _id: clientSummaryObject._id }, { associations: { $elemMatch: { "user.id": screener?.createdBy } } }] });
                             if (!userAssociated) {
                                 let newAssociations = clientSummaryObject.associations;
                                 //                                print(newAssociations.length)
@@ -104,7 +105,7 @@ function dataAssociationWithEvent(clientCollection, userCollection, screenerColl
                                     db.getCollection(summaryCollection).updateOne({ _id: clientSummaryObject._id }, { $set: { associations: newAssociations } })
                                     print("User added in associations Successfully..")
                                 } catch (e) {
-                                    print(`Failure in updation of association of Client${client._id.toString()} with User:${screener ?.createdBy}`)
+                                    print(`Failure in updation of association of Client${client._id.toString()} with User:${screener?.createdBy}`)
                                     print(e);
                                 }
                             } else {
@@ -112,21 +113,22 @@ function dataAssociationWithEvent(clientCollection, userCollection, screenerColl
                             }
                         } else {
                             //                            print("insert a new summary object for clientTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT");
-                            var dataAssociationSummaryDoc = {
-                                "client": dataAssociationDetailDoc.client,
+                            let dataAssociationSummaryDoc = {
+                                "client": screenerDetailObject.client,
                                 "associations": [],
-                                "createdBy": dataAssociationDetailDoc.createdBy,
-                                "createdAt": dataAssociationDetailDoc.createdAt,
-                                "lastModifiedBy": dataAssociationDetailDoc.lastModifiedBy,
-                                "lastModifiedAt": dataAssociationDetailDoc.lastModifiedAt,
-                                "dataAudit": dataAssociationDetailDoc.dataAudit
+                                "createdBy": screenerDetailObject.createdBy,
+                                "createdAt": screenerDetailObject.createdAt,
+                                "lastModifiedBy": screenerDetailObject.lastModifiedBy,
+                                "lastModifiedAt": screenerDetailObject.lastModifiedAt,
+                                "dataAudit": screenerDetailObject.dataAudit
                             }
-                            var associationDoc = {
-                                "user": dataAssociationDetailDoc.user,
-                                "assocDate": dataAssociationDetailDoc.assocDate,
-                                "status": dataAssociationDetailDoc.status
+                            let associationDoc = {
+                                "user": screenerDetailObject.user,
+                                "assocDate": screenerDetailObject.assocDate,
+                                "status": screenerDetailObject.status
                             }
                             dataAssociationSummaryDoc.associations.push(associationDoc);
+                            dataAssociationSummaryDocumentsList.push(dataAssociationSummaryDoc);
                         }
 
                     } else {
@@ -140,7 +142,19 @@ function dataAssociationWithEvent(clientCollection, userCollection, screenerColl
             }
         });
 
-        print(`Processed client from ${skipValue} to ${skipValue + batchSize}`)
+        let batchEndValue = skipValue+batchSize <= totalScreeners?skipValue+batchSize:totalScreeners;
+        try {
+            let detailResponse = db.getCollection(detailCollection).insertMany(dataAssociationDetailedDocumentsList);
+            let summaryResponse = db.getCollection(summaryCollection).insertMany(dataAssociationSummaryDocumentsList);
+            print(`${Object.values(detailResponse.insertedIds).length} documents inserted into ${detailCollection} collection`);
+            print(`${Object.values(summaryResponse.insertedIds).length} documents inserted into ${summaryCollection} collection`);
+            print(`Data inserted successfully for Batch Number: ${skipValue} to ${batchEndValue}`);
+        } catch (e) {
+            print(`Unalbe to insert records for ${skipValue} to ${batchEndValue}`);
+            print(e);
+        } 
+
+        print(`Processed client from ${skipValue} to ${batchEndValue}`)
     }
 }
 
