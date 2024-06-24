b15f62aeea8fa5db9c79281fcce1d2db08922e53
Tasawar Abbas <tabbas@stellatechnology.com>
2024-06-24 18:46:04 +0500

    Referral packat script

---
+++ Final Scripts/REFERRAL_PACKET.js
@@ -0,0 +1,6 @@
+const referralCollection = "Tasawar_client_referral_package";
+db.getCollection(referralCollection).createIndex({ updatedAt: 1 });
+db.getCollection(referralCollection).updateMany(
+   { updatedAt: { $exists: true } },
+   [{ $set: { lastModifiedAt: "$updatedAt" } }]
+)
