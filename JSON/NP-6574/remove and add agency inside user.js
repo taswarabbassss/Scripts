aea17832b94a111c89327fc0ad5f77efcf0de162
Tasawar Abbas <tabbas@stellatechnology.com>
2024-08-27 13:46:56 +0500

    db updte scripts

---
+++ JSON/NP-6574/remove and add agency inside user.js
@@ -0,0 +1,65 @@
+//1) removal agency from outside user document
+
+db.getCollection("test_data_association_summary").updateMany(
+  { "associations.agency": { $exists: true } },
+  { $unset: { "associations.$[].agency": "" } }
+);
+
+// 2) adding agency inside user
+
+const clientCollection = "data_association_summary";
+
+const allUsers = db
+  .getSiblingDB("qa-shared-ninepatch-user")
+  .getCollection("user")
+  .find(
+    {},
+    {
+      firstName: 1,
+      lastName: 1,
+      middleName: 1,
+      agency: 1,
+      defaultTenantId: 1,
+      tenantIds: 1,
+    }
+  )
+  .toArray()
+  .reduce((accumilator, user) => {
+    accumilator[user._id + ""] = user;
+    return accumilator;
+  }, {});
+
+print(allUsers.length);
+
+const summarryDocuments = db.getCollection(clientCollection).find({}).toArray();
+let index = 0;
+summarryDocuments.forEach((doc) => {
+  let updatedAssociations = [];
+  doc.associations.forEach((userDoc) => {
+    const agency = allUsers[userDoc.user.id].agency;
+    try {
+      userDoc.user.agency = {
+        _id: agency?._id,
+        name: agency?.name,
+        nickname: agency?.nickname,
+      };
+    } catch (error) {
+      print("ERROR IN ID: " + doc._id);
+      prit(error);
+    }
+    updatedAssociations.push(userDoc);
+    print(".");
+  });
+  doc.associations = updatedAssociations;
+  try {
+    db.getCollection(clientCollection).updateOne(
+      { _id: doc._id },
+      { $set: { associations: updatedAssociations } }
+    );
+  } catch (error) {
+    print("ERROR IN ID: " + doc._id);
+    prit(error);
+  }
+  print("# " + index);
+  index++;
+});
